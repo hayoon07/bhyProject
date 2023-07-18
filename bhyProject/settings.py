024902675ff9d1b0376b0810fee97649571cfc15
hayoon07 <backhay0714@naver.com>
2023-07-18 16:28:12 +0900

    secret key

--- bhyProject/settings.py
+++ bhyProject/settings.py
@@ -22,19 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-secret_file = os.path.join(BASE_DIR, 'secrets.json')
-
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
-
-def get_secret(setting, secrets = secrets):
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
-    
-SECRET_KEY = get_secret("SECRET_KEY")
+SECRET_KEY = 'django-insecure-f6)so138%m@*$r2u)&equ=e3f+&6-ys9(ps+$@0#iq8$1_uvw8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
